c55bd342ed5c9c64ac7e40b880abb393bf255298
royprince12-code <royprinceveigas031@gmail.com>
2026-09-04 11:47:23 +0530

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SIH_prototype/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
